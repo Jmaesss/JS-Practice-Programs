d2b980c34103be48f3c8130fd1cbbe50e3b297d7
Jmaesss <justinduarte05@gmail.com>
2025-10-20 21:40:07 +0800

    Add JsDrumKit/

--- JsDrumKit/main.js
+++ JsDrumKit/main.js
@@ -1,3 +1,23 @@
-window.addEventListener("keydown", e => {
-    const audio = document.querySelector(`audio[data-key="${e.keyCodde}"]`)
-})
+
+function playSound(e){
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if(!audio){
+        return; // stop function from running
+    }
+    audio.currentTime = 0
+    audio.play();
+    key.classList.add('playing');
+
+}
+
+
+function removeTransition(e) {
+    if(e.propertyName !== 'transform') return; // skips it if its not a transform
+    this.classList.remove('playing');
+    console.log(this)
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener("keydown", playSound)
